49cfb0ae4c65e37b6e94a2c06cf116e328f512c7
monam2 <kangcw0107@gmail.com>
2025-02-09 23:24:02 +0900

    add button 스타일 수정

--- src/components/common/addButton/AddButton.tsx
+++ src/components/common/addButton/AddButton.tsx
@@ -7,7 +7,7 @@ export default function AddButton({
 }: AddButtonProps) {
   return (
     <div
-      className="w-[40px] h-[28px] bg-add-button-bg text-add-button-text rounded-2xl flex items-center justify-center"
+      className="w-[40px] h-[28px] bg-add-button-bg text-add-button-text rounded-2xl flex items-center justify-center cursor-pointer"
       onClick={onClick}
     >
       <img
