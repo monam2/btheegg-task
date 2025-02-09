764d9bea85eb6c0d1d7d6339a7d97058f406f67c
monam2 <kangcw0107@gmail.com>
2025-02-09 20:35:29 +0900

    empty card 스타일 수정

--- src/components/common/emptyCard/EmptyCard.tsx
+++ src/components/common/emptyCard/EmptyCard.tsx
@@ -2,7 +2,7 @@ import AddButton from "../addButton/AddButton";
 
 export default function EmptyCard() {
   return (
-    <div className="w-[200px] h-[130px] flex flex-col justify-center items-center gap-2 bg-none-card rounded-lg p-4 cursor-pointer shadow-md">
+    <div className="w-[200px] h-[130px] flex flex-col justify-center items-center gap-2 bg-add-button-bg rounded-lg p-4 cursor-pointer shadow-md">
       <p className="text-sm font-pretendard-regular text-gray-100">
         지금 바로 추가해보세요.
       </p>
