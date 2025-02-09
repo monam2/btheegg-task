725cb7ee7eccc58dd2a4799ed1abec2f5ca40e62
monam2 <kangcw0107@gmail.com>
2025-02-09 23:24:11 +0900

    프로필 컴포넌트 반응형 추가

--- src/components/common/Profile/Profile.tsx
+++ src/components/common/Profile/Profile.tsx
@@ -1,15 +1,23 @@
-export default function Profile({ name = "홍길동" }: { name?: string }) {
+export default function Profile({
+  name = "강창우",
+}: {
+  name?: string;
+}) {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-2 sm:gap-4 md:gap-6">
       <img
         src="/profile.svg"
         alt="profile"
-        className="w-[36px] h-[36px] rounded-full mr-1"
+        className="w-[36px] h-[36px] rounded-full mr-1 sm:mr-2 md:mr-3"
       />
-      <p className="text-sm font-pretendard-semi-bold text-gray-500 pr-5">
+      <p className="text-sm font-pretendard-semi-bold text-gray-500 pr-5 sm:pr-6 md:pr-8">
         {name}님
       </p>
-      <img src="/arrow.svg" alt="arrow" />
+      <img
+        src="/arrow.svg"
+        alt="arrow"
+        className="hidden sm:block"
+      />
     </div>
   );
 }
